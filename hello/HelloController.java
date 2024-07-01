022ad5613d231ed343bd9f1fa1f2a9b84608583a
Chinatsu-cloud <hiyohiko720@gmail.com>
2024-07-01 17:41:25 +0900

    test:指定したリクエストパラメータを取得できるようにしました。

--- hello/HelloController.java
+++ hello/HelloController.java
@@ -1,13 +1,15 @@
 package com.hello.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 @RestController
 public class HelloController{
     @GetMapping("/hellohello")//リンクの一番後ろにつけるキー？パス
-    public String hello(){//メソッド名はなんでもいいが実態を表す名前が良い
-        System.out.println("hello and welcom!");
-        return "Hello　World!";
+    public String index(
+            @RequestParam(name="name",value = "name",defaultValue = "no name",required = false)String name,
+            @RequestParam(name = "id")String id){
+                return "ID:"+id+"NAME:"+name;
     }
 }
